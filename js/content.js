fd2740c83b37a5f83f5869718e27555106027109
kobake <kobake@users.sourceforge.net>
2013-05-04 04:49:53 +0900

    version 1.1 step2

--- js/content.js
+++ js/content.js
@@ -25,10 +25,11 @@ Node.prototype.insertAfter = function (node, referenceNode) {
 };
 
 // -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- //
-// リクエスト受付
+// リクエスト受付 (メニュー応答)
 // -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- //
 var g_orgBody = '';
 $(function () {
+	if (!chrome.extension) return;
 	chrome.extension.onRequest.addListener(function (request, sender, callback) {
 		// 手動置換
 		if (request.action == 'replace') {
@@ -66,11 +67,13 @@ function RunReplaceElement() {
 	ReplaceElement(document.body);
 }
 
-
+var g_test = 0;
 var g_timing = 0;
 var g_way = 0;
 var g_colorStyle = '';
-var g_reg = new RegExp("(" + g_nengo_reg + ")[ \\t\\r\\n]*([0-9０-９一二三四五六七八九十百千元]+)(年?)", 'g');
+var g_regStr = "(" + g_nengo_reg + ")[ \\t\\r\\n]*([0-9０-９一二三四五六七八九十百千元]+)(年?)";
+var g_regAll = new RegExp(g_regStr, 'g');
+var g_regOnce = new RegExp(g_regStr);
 // content起動時に呼ばれる関数
 // localStorageの値を読み取り、設定に応じてページ置換処理を行う
 $(function () {
@@ -85,8 +88,9 @@ function NegoReplacer() {
 		}
 	});
 
-	// ###テスト用
-	if (0) {
+	// テスト用
+	if (g_test) {
+		g_timing = 1;
 		g_way = 2;
 		g_colorStyle = "color:red; background-color:#aff";
 		for (var i = 0; i < 3; i++) {
@@ -112,17 +116,24 @@ function NegoReplacer() {
 
 // Element置換
 // 再帰的に子供を漁る
+var g_pool = '';
+var g_poolElement = null;
 function ReplaceElement(element) {
 	var childs = element.childNodes;
 	for (var i = 0; i < element.childNodes.length; i++) {
 		var t = element.childNodes[i];
 		if (t.nodeName == "#comment") continue;
 		if (t.nodeName == "#text") {
-			var new_t = ReplaceNengo(t.data);
-			if (new_t != t.data) {
+			//console.log(t.data);
+			// テキスト置換
+			var old_t = t.data;
+			var new_t = ReplaceNengo(old_t);
+			g_pool = '';
+			// 置換に成功していたら、タグ入れ替えを行う
+			if (new_t != old_t) {
 				var e = document.createElement("span");
 				e.innerHTML = new_t;
-				if (1) {
+				if (0) {
 					for (var k = e.childNodes.length - 1; k >= 0; k--) {
 						element.insertAfter(e.childNodes[k], t);
 						i++;
@@ -133,7 +144,16 @@ function ReplaceElement(element) {
 				else {
 					element.replaceChild(e, t);
 				}
+				// 次の置換用に後ろ4文字を残しておく
+				g_pool = new_t.substr(-4);
+				g_poolElement = e;
 			}
+			else {
+				// 次の置換用に後ろ4文字を残しておく
+				g_pool = old_t.substr(-4);
+				g_poolElement = t;
+			}
+			console.log("pool = " + g_pool);
 		}
 		else {
 			ReplaceElement(t);
@@ -141,9 +161,59 @@ function ReplaceElement(element) {
 	}
 }
 
-// 文字列置換
+// 文字列置換 (strに含まれるもの全部)
+var g_hitCount = 0;
+var g_lastPos = 0;
+var g_lastAfter = '';
 function ReplaceNengo(str) {
-	return str.replace(g_reg, RepCallback);
+	g_hitCount = 0;
+	var normalStartPos = 0;
+	// 先頭置換 (プール考慮)
+	if (g_pool.length) {
+		var tmp = g_pool + str;
+		var tmp2 = tmp.replace(g_regOnce, RepCallback);
+		if (g_hitCount > 0) {
+			// プール影響の評価
+			if (g_lastPos < g_pool.length) {
+				// 今回いらないところを削る
+				tmp2 = tmp2.substr(g_lastPos);
+				// 前回のいらないところを削る
+				if (g_poolElement) {
+					console.log("culPrev: " + g_poolElement.innerHTML + " (" + g_poolElement.nodeName + ")");
+					var s;
+					if (g_poolElement.innerHTML != null) {
+						s = g_poolElement.innerHTML;
+					}
+					else if (g_poolElement.data != null) {
+						s = g_poolElement.data;
+					}
+					var n = 4;if (s.length < n) n = s.length;
+					s = s.substr(0, s.length - (n - g_lastPos));
+					if (g_poolElement.innerHTML != null) {
+						g_poolElement.innerHTML = s;
+					}
+					else if (g_poolElement.data != null) {
+						g_poolElement.data = s;
+					}
+				}
+			}
+			else {
+				// プールは必要なかったので今回のプールを削る
+				tmp2 = tmp2.substr(g_pool.length);
+			}
+			// 通常置換の開始位置
+			normalStartPos = g_lastPos + g_lastAfter.length;
+			// 結果
+			str = tmp2;
+		}
+		else {
+			// そもそもヒットしてないのでそのまま返す
+			return str;
+		}
+	}
+	// 通常置換 (プール非考慮)
+	var ret = str.substr(0, normalStartPos) + str.substr(normalStartPos).replace(g_regAll, RepCallback);
+	return ret;
 }
 
 /*
@@ -151,6 +221,8 @@ function ReplaceNengo(str) {
 	"昭和三2年" のような文字列を受け取り置換する
 */
 function RepCallback(match_text, nengo, num, nen, pos, self) {
+	g_hitCount++;
+	g_lastPos = pos;
 	// 数値変換
 	num = parseInt(num.replace(/./g, ReplaceNumCallback));
 	// 年号解釈
@@ -181,6 +253,7 @@ function RepCallback(match_text, nengo, num, nen, pos, self) {
 	if (g_colorStyle != '') {
 		ret += '</span>';
 	}
+	g_lastAfter = ret;
 	return ret;
 }
 
